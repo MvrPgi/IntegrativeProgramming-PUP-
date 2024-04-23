b5e8246c57f164bf26df390a7b71e9ecf691bf25
Mover <movermercene@gmail.com>
2024-04-23 15:49:28 +0800

    23.04.24

--- Groupings/PHYTON_PROGRAM_GROUP/ACT_2.py
+++ Groupings/PHYTON_PROGRAM_GROUP/ACT_2.py
@@ -1,6 +1,4 @@
 
-
-
 #m   Studio Type  P65, 892.00 per sqm
 #52.0 sqm to below 86.5 sqm   2 Bedroom    P58, 807.00 per sqm
 #86.5 sqm and above           3 Bedroom    P53, 380.00 per sqm
@@ -27,20 +25,40 @@ def area():
   studio = ("Studio Type")
   bed = (" 2 Bed Room")
   bed2 ="3 Bed Room,"
-  P1 = 65892.00
-  P2 = 58807.00
-  P3 = 53380.00
+  P1 = 65892.00*area
+  P2 = 58807.00*area
+  P3 = 53380.00*area
+
   if area < 52.0:
     print("Unit Type: ",studio) 
-    print(f"Total Price: P{P1*area}") 
+    print(f"Total Price: P{P1}") 
+    return P1
   elif  area  <= 86.7:
     print("Unit Type: ",bed)
-    print(f"Total Price P{P3*area}")
-
+    print(f"Total Price P{P3}")
+    return P2
   elif area <= 86.5:
     print("Unit Type: ",bed2) 
-    print(f"Total Price: P{P2*area}") 
- 
-area()
+    print(f"Total Price: P{P2}") 
+    return P3
+    
 
+
+def downpayment(total_price):
+  downpayment=float(input("Downpayment Amount: "))
+  if downpayment < 0.2 * total_price:
+        print("No discount applied.")
+  elif 20 <= downpayment < 30:
+        print("3% discount applied.")
+  elif 30 <= downpayment < 40:
+        print("4% discount applied.")
+  else:
+        print("5% discount applied.")
+  print(f"BALANCE {total_price - downpayment}:")
+
+
+
+
+area()
+downpayment()
 
