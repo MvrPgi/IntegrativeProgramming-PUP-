dc33a1f60f61b3fec7561198c5772228766d17f6
Mover <movermercene@gmail.com>
2024-05-14 01:41:16 +0800

    14.5.24

---
+++ LESSONS/functions.py
@@ -0,0 +1,28 @@
+#A function is a block of code which only runs when it is called.
+
+#def my_function():
+#  print("Hello from a function")
+
+# my_function()
+#_________________________________________________________________________________________________________________________________________________________
+# Global variables
+# x = 500
+# def myfunc():
+#  global x
+#  x = 300
+# myfunc()
+# print(x)
+
+# Local variables - A variable created inside a function belongs to the local scope of that function, and can only be used inside that function.
+#_________________________________________________________________________________________________________________________________________________________
+
+
+# Function Arguments
+# Required arguments - Required arguments are the argumentspassed to a function in correct positional order. 
+# Keyword arguments - Keyword arguments are related to the function calls. When you use keyword arguments in a function call, the caller identifies the arguments by the parameter name.
+# Default arguments - A default argument is an argument that assumes a default value if a value is not provided in the function call for that argument.
+# Variable-length arguments - You may need to process a function for more arguments than you specified while defining the function. These arguments are called variable-length arguments and are not named in the function definition, unlike required and default arguments.
+#_________________________________________________________________________________________________________________________________________________________
+#def my_function(fname, lname):  #A  parameter is the variable listed inside theparentheses in the function definition.
+#  print(fname + " " + lname)
+#my_function("James", "Nayre") # An argument is the value that is sent to the function when it is called.
