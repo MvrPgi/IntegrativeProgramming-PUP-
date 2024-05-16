bb02a40e36a7b78effe59d783ceded1099ccefab
Mover <movermercene@gmail.com>
2024-05-16 18:07:25 +0800

    16.05.24

--- LESSONS/modules.py
+++ LESSONS/modules.py
@@ -3,3 +3,18 @@
 def greeting(name):
   	print("Hello, " + name)
 	
+
+def greeting(name):
+    print("Hello " + name)
+    
+def add(x, y):
+    return (x+y)
+
+def subtract(x, y):
+    return (x-y)
+
+def multiply(x, y):
+    return (x*y)
+
+def divide(x, y):
+    return (x/y)
--- LESSONS/modules1.py
+++ LESSONS/modules1.py
@@ -1,3 +1,11 @@
 import modules   
 
 modules.greeting("Justine Biber")
+
+modules.greeting("Rachel")
+x=2
+y=7
+print(modules.add(x,y))
+print(modules.subtract(x,y))
+print(modules.multiply(x,y))
+print(modules.divide(x,y))
